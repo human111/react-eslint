c4c3fe5c93ca34f055f2ece46742c167f0139499
guohongyu04 <guohongyu04@meituan.com>
2020-07-29 10:30:16 +0800

    style:添加了一些关键字的备注

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,21 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    // 'plugin:react/recommended',
+    'plugin:react/recommended',
     'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint', // 词法解析器使用babel-eslint，以更好的适配es6的新api
     ecmaFeatures: {
         'jsx': true
     },
-    ecmaVersion: 11,
+    ecmaVersion: 6, // 启用 ES6 语法支持;默认设置为3，5（默认）， 你可以使用 6、7、8 或 9 来指定你想要使用的 ECMAScript 版本。
     sourceType: 'module'
   },
   plugins: [
     'react'
   ],
+  // 可以在这里覆盖react的规则
   rules: {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
---
+++ README.md
@@ -0,0 +1,7 @@
+# 插件描述
+
+ESlint的重心在代码质量，Prettier只关心代码格式
+
+eslint-plugin-react：检测react中的jsx语法
+
+eslint-config-prettier 使用 eslint 兼容 Prettier 的规则
